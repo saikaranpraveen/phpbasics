6f49e998cac2db3e3771ca760bf03384f0be055a
KARAN PRAVEEN <karansiamintern@gmail.com>
2021-04-19 14:50:04 +0530

    For loop to count upto 20 in increment of 2

---
+++ For_loop.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php  
+echo "This counts from 0 to 20 in increment of two.";
+echo "<br>";
+for ($x = 0; $x <= 20; $x+= 2) {
+  echo " $x <br>";
+}
+?>  
+
+</body>
+</html>
