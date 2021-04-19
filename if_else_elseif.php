9aec2b741188b9407edd5a053d9656bcad12ae46
KARAN PRAVEEN <karansiamintern@gmail.com>
2021-04-19 15:00:47 +0530

    If, else and elseif functions

---
+++ if_else_elseif.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$a = 10;
+$b = 20;
+
+
+echo "The two numbers are $a and $b.";
+echo "<br>";
+
+if ($a < $b ) 
+{
+  echo "$a is lesser than $b.";
+} 
+elseif ($b < $a) 
+{
+  echo "$b is lesser than $a.";
+} 
+else 
+{
+  echo "They are equal";
+}
+?>
+ 
+</body>
+</html>
