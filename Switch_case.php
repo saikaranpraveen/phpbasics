29091a6a71b185d907e4e1422d6ae5e761fe9d36
KARAN PRAVEEN <karansiamintern@gmail.com>
2021-04-19 14:55:29 +0530

    Using switch case to know your favourite actor

---
+++ Switch_case.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$favactor = null;
+
+switch ($favactor) {
+  case "Cruise":
+    echo "Your favorite actor is Tom Cruise!";
+    break;
+  case "Pitt":
+    echo "Your favorite actor is Brad Pitt!";
+    break;
+  case "Jolie":
+    echo "Your favorite actor is Angelina Jolie!";
+    break;
+  default:
+    echo "You have bad taste!";
+}
+?>
+ 
+</body>
+</html>
