d50b57f1be121f760f4e50a2c6dc8dfe63a33a63
KARAN PRAVEEN <karansiamintern@gmail.com>
2021-04-19 14:59:32 +0530

    Iterating a number

---
+++ While_loop.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php  
+$a = 1;
+echo "The number keeps iterating by one from 1 till 10";
+ 
+while($a <= 10) {
+  echo "<br>";
+  echo $a;
+ 
+  $a++;
+} 
+?>  
+
+</body>
+</html>
