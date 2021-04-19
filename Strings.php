fbac50393f35635a0c63667d02b9ce42cf46a9cb
KARAN PRAVEEN <karansiamintern@gmail.com>
2021-04-19 14:54:01 +0530

    Functions to manipulate strings

---
+++ Strings.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo strlen("Hello world!");
+echo "<br>";
+echo str_word_count("Hello world!");
+echo "<br>";
+echo strrev("Hello world!"); 
+echo "<br>";
+echo str_replace("Hello", "Bye", "Hello world!");
+?> 
+ 
+</body>
+</html>
